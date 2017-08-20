7867a013f171fad0610b3fe4e23ed896d125c2a8
Madunagu Ekene <ekenemadunagu@gmail.com>
2017-08-20 08:44:55 +0100

    used custom settings layout

--- app/src/main/java/com/fedironics/imeter/imetercustomer/SettingsFragment.java
+++ app/src/main/java/com/fedironics/imeter/imetercustomer/SettingsFragment.java
@@ -1,20 +1,10 @@
 package com.fedironics.imeter.imetercustomer;
 
-import android.app.Application;
-import android.content.SharedPreferences;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.preference.RingtonePreference;
+
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,29 +16,15 @@ import org.json.JSONObject;
  */
 
 
-public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsFragment extends Fragment{
+    public     View settingsFragment;
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.settings_fragment);
-        getPreferenceScreen().getSharedPreferences()
-                .registerOnSharedPreferenceChangeListener(this);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        settingsFragment = inflater.inflate(R.layout.fragment_profile,container,false);
+        return settingsFragment;
     }
 
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-                                          String key) {
-        Log.e(iMeterApp.TAG,"a preference was changed");
-        if (key.equals("customer_number")) {
-            Preference connectionPref = findPreference(key);
-            // Set summary to be the user-description for the selected value
-            connectionPref.setSummary(sharedPreferences.getString(key, ""));
-            iMeterApp myApp = (iMeterApp) getActivity().getApplicationContext();
-                myApp.getNepaInfo(sharedPreferences.getString(key, ""));
 
-        }
-
-    }
 
 }
